959045579d484cdb0f9138a71bf97197a476e663
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-04 13:45:34 +0600

    chef details data added

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const port = process.env.PORT || 5000;
 
 
 const chefs = require('./data/chefs.json');
+const chefData = require('./data/chefData.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send(chefs);
+})
+app.get('/chefData', (req, res) => {
+  res.send(chefData);
+})
+
+app.get('/chefData/:id', (req, res) => {
+  const id = req.params.id;
+  const selected = chefData.find(n => n.id === id)
+  res.send(selected);
 })
 
 app.listen(port, () => {
